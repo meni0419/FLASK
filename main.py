38e54f95ed5134083e70f5cafb662dff1d824952
meni0419 <meni0419@gmail.com>
2025-06-12 10:08:30 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return 'Hello, World!'
+
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    return f'User {username}'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
